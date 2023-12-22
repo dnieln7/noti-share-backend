f816fc693d9df1adc24db293e331aec38b2069ee
Daniel <dnieln7@gmail.com>
2023-12-22 17:37:41 -0600

    Each request will only have 1 destination, if the token is wrong 404 response is returned

--- internal/handler/notification/post_notification_handler.go
+++ internal/handler/notification/post_notification_handler.go
@@ -11,13 +11,13 @@ import (
 )
 
 type PostNotificationBody struct {
-	Title        string   `json:"title"`
-	Content      string   `json:"content"`
-	Owner        string   `json:"owner"`
-	Origin       string   `json:"origin"`
-	Timestamp    uint64   `json:"timestamp"`
-	Version      uint8    `json:"version"`
-	Destinations []string `json:"destinations"`
+	Title       string `json:"title"`
+	Content     string `json:"content"`
+	Owner       string `json:"owner"`
+	Origin      string `json:"origin"`
+	Timestamp   uint64 `json:"timestamp"`
+	Version     uint8  `json:"version"`
+	Destination string `json:"destination"`
 }
 
 func PostNotificationHandler(writer http.ResponseWriter, request *http.Request, resources *server.Resources) {
@@ -40,42 +40,42 @@ func PostNotificationHandler(writer http.ResponseWriter, request *http.Request, 
 
 	var invalidTokens []string
 
-	for _, token := range body.Destinations {
-		message := &messaging.Message{
-			Data: map[string]string{
-				"title":     body.Title,
-				"content":   body.Content,
-				"owner":     body.Owner,
-				"origin":    body.Origin,
-				"timestamp": fmt.Sprintf("%d", body.Timestamp),
-				"version":   fmt.Sprintf("%d", body.Version),
-			},
-			Token: token,
-		}
-
-		_, err := resources.FirebaseMessaging.Send(request.Context(), message)
-
-		if err != nil {
-			log.Println("Error sending message:", err)
-
-			errString := fmt.Sprintf("%v", err)
-
-			if errString == "Requested entity was not found." || errString == "The registration token is not a valid FCM registration token" || errString == "exactly one of token, topic or condition must be specified" {
-				invalidTokens = append(invalidTokens, token)
-			}
-		} else {
-			log.Println("Success sending message")
-		}
+	message := &messaging.Message{
+		Data: map[string]string{
+			"title":     body.Title,
+			"content":   body.Content,
+			"owner":     body.Owner,
+			"origin":    body.Origin,
+			"timestamp": fmt.Sprintf("%d", body.Timestamp),
+			"version":   fmt.Sprintf("%d", body.Version),
+		},
+		Token: body.Destination,
 	}
 
-	if len(invalidTokens) > 0 {
-		response := struct {
-			Status        string   `json:"status"`
-			Reason        string   `json:"reason"`
-			InvalidTokens []string `json:"invalid_tokens"`
-		}{Status: "Failure", Reason: "INVALID_TOKEN", InvalidTokens: invalidTokens}
+	_, err = resources.FirebaseMessaging.Send(request.Context(), message)
 
-		helper.ResponseJson(writer, 400, response)
+	if err != nil {
+		log.Println("Error sending message:", err)
+
+		errString := fmt.Sprintf("%v", err)
+
+		if errString == "Requested entity was not found." || errString == "The registration token is not a valid FCM registration token" || errString == "exactly one of token, topic or condition must be specified" {
+			response := struct {
+				Status        string   `json:"status"`
+				Reason        string   `json:"reason"`
+				InvalidTokens []string `json:"invalid_tokens"`
+			}{Status: "Failure", Reason: "INVALID_TOKEN", InvalidTokens: invalidTokens}
+
+			helper.ResponseJson(writer, 404, response)
+		} else {
+			response := struct {
+				Status        string `json:"status"`
+				Reason        string `json:"reason"`
+				FirebaseError string `json:"firebase_error"`
+			}{Status: "Failure", Reason: "FIREBASE_ERROR", FirebaseError: errString}
+
+			helper.ResponseJson(writer, 500, response)
+		}
 	} else {
 		helper.ResponseNoContent(writer)
 	}
